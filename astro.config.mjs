23566d911e54e518df19172f1a35bac14faaae75
destino8k <perikidesti87@gmail.com>
2024-12-30 01:51:59 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import node from '@astrojs/node';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
